231271b595057652b0d225c44b91ffef731a3d93
santinogalliano <gallianosantino@gmail.com>
2025-04-17 11:28:38 -0300

    Limpiando warnings 2

--- TP2-x86_64/ej1_u/ej1.c
+++ TP2-x86_64/ej1_u/ej1.c
@@ -20,7 +20,7 @@ string_proc_node* string_proc_node_create(uint8_t type, char* hash){
 
 void string_proc_list_add_node(string_proc_list* list, uint8_t type, char* hash){
 	string_proc_node* node = string_proc_node_create(type, hash);
-	if (node == NULL) return;
+	if (node == NULL) return NULL;
 	if (list->first == NULL) {
 		list->first = node;
 		list->last = node;
